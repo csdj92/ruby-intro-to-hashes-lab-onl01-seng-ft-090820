44468093392f3defd1547857ae9e5e4907af2dd8
CJ Davis <csdj92@gmail.com>
2020-09-11 16:20:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,8 +32,14 @@ monopoly = {:railroads => {
   :rent_in_dollars=>
    {
     :one_piece_owned=>25,
+    :two_pieces_owned=>50,
+    :three_pieces_owned=>100,
+    :four_pieces_owned=>200
     }
-  :names=> {}
+  :names=> {
+    :reading_railroad=>{},
+    
+  }
 }}
 end
 
